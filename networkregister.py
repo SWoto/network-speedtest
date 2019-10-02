71703296b99642f1670980233f7c946760ad0e59
SWoto <brunostinghel137@gmail.com>
2019-10-02 01:13:53 +0000

    Update networkregister.py

--- networkregister.py
+++ networkregister.py
@@ -16,20 +16,21 @@ def checkFolderOrFile(pathFile):
         return False
 
     
-def haveInternet(logger):
+def haveInternet():
     conn = httplib.HTTPConnection("www.google.com", timeout=5)
     try:
         conn.request("HEAD", "/")
         conn.close()
-        logging.debug('Successful Request')
+        #logging.debug('Successful Request')
         return True
     except Exception as e:
         conn.close()
-        logging.exception('Request Failure')
+        print(e)
+        #logging.exception('Request Failure')
         return False
 
     
-def test(logger):
+def test():
     try:
         s = speedtest.Speedtest()
         s.get_servers()
@@ -37,56 +38,60 @@ def test(logger):
         s.download()
         s.upload()
         res = s.results.dict()
-        logging.debug('Speedtest successfully made')
+        #logging.debug('Speedtest successfully made')
         return res.get("download"), res.get("upload"), res.get("ping")
     except Exception as e:
-        logging.exception('Speedtest Failure')
+        #logging.exception('Speedtest Failure')
+        print(e)
         return None        
 
 
-def textReport(logger, path,file):
+def textReport(path, file):
     # write header to new csv
     if not(checkFolderOrFile(path+file)):
         try:
             with open(path+file, 'w') as f:
                 f.write('timestamp, status, download [MB/s], upload[MB/s], ping [ms]\n')
-                logger.debug('The header has been written.')
+                #logger.debug('The header has been written.')
         except Exception as e:
-            logger.exception('Couldn\'t write the header into file.')
+            #logger.exception('Couldn\'t write the header into file.')
+            print(e)
         
     #write data into csv        
     with open(path+file, 'a') as f:
         ts = dt.now()
-        if haveInternet(logger):
-            print('Testing...')
+        if haveInternet():
+            print('{}: Testing...'.format(ts))
             try:
-                d, u, p = test(logger)
+                d, u, p = test()
                 f.write('{};connected;{:.2f};{:.2f};{:.2f}\n'.format(ts, d / 1024 / 1024, u / 1024 / 1024, p))
-                logger.debug('Speedtest has been written.')
+                #logger.debug('Speedtest has been written.')
             except Exception as e:
                 f.write('{};disconnected;{:.2f};{:.2f};{}\n'.format(ts, 0., 0., 'inf'))
-                logger.exception('Couldn\'t execute/write speedtest.')
+                print(e)
+                #logger.exception('Couldn\'t execute/write speedtest.')
         else:
             try:                  
                 f.write('{};disconnected;{:.2f};{:.2f};{}\n'.format(ts, 0., 0., 'inf'))
-                logger.debug('Current Status has been written.')
+                #logger.debug('Current Status has been written.')
             except Exception as e:
-                logger.exception('Couldn\'t write into file.')
+                print(e)
+                #logger.exception('Couldn\'t write into file.')
 
 
-def main(logger, path, file):
+def main(path, file):
     #run every five minutes = 300 seconds
-    threading.Timer(300.0, main, [logger, path, file]).start()  
-    textReport(logger, path, file)
+    threading.Timer(300.0, main, [path, file]).start()  
+    textReport(path, file)
       
 dir = os.path.dirname(__file__)
 file = 'speedhistory.csv'
 out_path = os.path.join(dir, file)
-log_path = os.path.join(dir, 'speedtest.log')
+#log_path = os.path.join(dir, 'speedtest.log')
 print(out_path)
-logging.basicConfig(filename=log_path, filemode='a+', format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+#logging.basicConfig(filename=log_path, filemode='a+', format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 # Get the logger specified in the file
 
 
 
-main(logging, out_path, file)
+main(out_path, file)
