0dfdeeea025f3234eb1fdf98464dd04592642ce2
Bruno <brunostinghel137@gmail.com>
2020-06-27 16:01:32 -0300

    now storing in shelv db instead of csv, changed threadjob to sched

---
+++ classes/classLogger.py
@@ -0,0 +1,22 @@
+import logging
+from datetime import datetime
+
+
+class EscritorDeLog():
+    """ Class that implement the logger"""
+    def __init__(self, *args, **kwargs):
+        formatter = logging.Formatter('[%(levelname)-8s] [%(asctime)s] - %(name)s:%(funcName)s:%(lineno)d - %(message)s', 
+            "%Y-%m-%d %H:%M:%S")
+        handler = logging.FileHandler('logs/networkspeedtest-{:%Y%m%d}.log'.format(datetime.now()), mode='a+')
+        handler.setFormatter(formatter)
+        self.escreve_log = logging.getLogger(self.__class__.__name__)
+        self.escreve_log.setLevel(logging.DEBUG)
+        self.escreve_log.handlers = []
+        self.escreve_log.addHandler(handler)
+
+        console = logging.StreamHandler()
+        console.setLevel(logging.WARNING)
+        console.setFormatter(formatter)
+        self.escreve_log.addHandler(console)
+
+        
--- networkregister.py
+++ networkregister.py
@@ -1,62 +1,34 @@
+import sched, time
 import os
-import sys
-import logging
-import logging.config
-from threadjob import ThreadJob
-from speedtest import Speedtest
-from datetime import datetime as dt
 import requests
+import json
+import shelve #persistent, dictionary-like object
+from classes.speedtest import Speedtest
+from classes.classLogger import EscritorDeLog
 
-
-def get_env_variable(var_name, default=None) -> str:
-    """Get the environment variable or return False"""
-    try:
-        return os.environ[var_name]
-    except KeyError:
-        return False
-
-
-def handle_exception(exc_type, exc_value, exc_traceback):
-    if issubclass(exc_type, KeyboardInterrupt):
-        sys.__excepthook__(exc_type, exc_value, exc_traceback)
-        return
-
-    logger.error("Uncaught exception", exc_info=(exc_type, exc_value, exc_traceback))
-
-sys.excepthook = handle_exception
-
-
-class NetworkSpeedTestRegister():
-    def __init__(self, host='http://www.google.com/', timeout = 5, filename = 'NetworkSpeedHistory.csv', log_name = 'basicLog'):
+class NetworkSpeedTestRegister(EscritorDeLog):
+    def __init__(self, host='http://www.google.com/', timeout = 5, shelvename = 'NetworkSpeedHistory'):
+        super().__init__()
         self.host = host
         self.timeout = timeout
-        self.filename = filename
-        self.speedtest = None
-        self.logger = logging.getLogger(log_name + '.' + type(self).__name__)
+        self.shelvename = shelvename
         self.thread_job = None
 
     def check_internet_connection(self) -> bool:
-        self.logger.info('Testing connection')
+        self.escreve_log.debug("Testing connection")
         #conn = httplib.HTTPConnection(self.host, timeout=self.timeout)
         try:
             _ = requests.get(self.host, timeout = self.timeout)
             return True
         except requests.ConnectionError:
-            self.logger.debug('The device is offline')
+            self.escreve_log.warning("The device is offline")
             return False
         except Exception:
-            self.logger.exception('Could\'t check connection')
-            return False
-
-    def check_path(self, path):
-        self.logger.info('Checking if path/file exist')
-        if os.path.exists(path):
-            return True
-        else:
+            self.escreve_log.exception('Could\'t check connection')
             return False
 
     def do_speedtest(self):
-        self.logger.info('Running SpeedTest')
+        self.escreve_log.debug('Running SpeedTest')
         try:
             self.speedtest = Speedtest()
             self.speedtest.get_servers()
@@ -65,56 +37,36 @@ class NetworkSpeedTestRegister():
             self.speedtest.upload()
             return True
         except Exception:
-            self.logger.exception('Speedtest Failure')
+            self.escreve_log.exception('Speedtest Failure')
             return False     
 
-    def do_test_report(self):
-        self.logger.info('Writing test report')
+    def do_test_report(self, schedulerExecutions):
+        self.escreve_log.debug('Writing test report')
         # write header to new csv
-        dir = os.path.dirname(__file__)
-        out_path = os.path.join(dir, self.filename)
-        if not(self.check_path(out_path)):
-            try:
-                with open(out_path, 'w') as f:
-                    f.write('timestamp, status, download [MB/s], upload[MB/s], ping [ms]\n')
-            except Exception:
-                self.logger.exception('Couldn\'t write header in file.')
-
-        #write data into csv
-        try:         
-            with open(out_path, 'a') as f:
-                ts = dt.now()
+        # dir = os.path.dirname(__file__)
+        # out_path = os.path.join(dir, self.filename)
+        
+        try:
+            with shelve.open(self.shelvename) as db:
                 if self.check_internet_connection() and self.do_speedtest():
-                    res = self.speedtest.results.dict()
-                    download = res.get("download")
-                    upload = res.get("upload")
-                    ping = res.get("ping")
-                    string = 'connected;{:.2f};{:.2f};{:.2f}\n'.format(download / 1024 / 1024, upload / 1024 / 1024, ping)
-                    f.write('{};{}'.format(ts, string))
-                    print('{};{}'.format(ts, string))
-                else:             
-                    f.write('{};disconnected;{:.2f};{:.2f};{}\n'.format(ts, 0., 0., 'inf'))
-                    print('{};disconnected;{:.2f};{:.2f};{}\n'.format(ts, 0., 0., 'inf'))
+                    # Serializing json  
+                    json_object = self.speedtest.results.dict()
+                else:  
+                    json_object = {"download": 0, "upload": 0,  "ping": 999}
+                    #json_object = json.dumps(dictionary)
+                key = str(int(time.time()))
+                db[key] = json_object
+            schedulerExecutions.enter(60, 1, self.do_test_report, (schedulerExecutions,))
         except Exception:
-                self.logger.exception('Couldn\'t write header in file.')
+            self.escreve_log.exception('Failed to write to db')
 
 
-    def set_threading(self, time) -> None:
-        self.logger.info('Starting Thread')
-        self.thread_job = ThreadJob(interval = time, execute = self.do_test_report)
-        self.thread_job.start()
+HOST = 'http://www.google.com/'
+TIMEOUT = 5
+SHELVNAME = 'database/NetworkSpeedHistory'
 
-    def stop_threading(self) -> None:
-        self.logger.info('Stoping Thread')
-        self.thread_job.stop()
+objSpeedTest = NetworkSpeedTestRegister(HOST, TIMEOUT, SHELVNAME)
 
-#create logger
-logging.config.fileConfig('logging.conf')
-logger = logging.getLogger('basicLog')
-
-file = 'NetworkSpeedHistory.csv'
-out_path = os.path.join(os.path.dirname(__file__), file)
-print('Saving tests in: {}'.format(out_path))
-
-NST = NetworkSpeedTestRegister(log_name = 'basicLog')
-NST.set_threading(20)
+objScheduler = sched.scheduler(time.time, time.sleep)
+objScheduler.enter(60, 1, objSpeedTest.do_test_report, (objScheduler,))
+objScheduler.run()
