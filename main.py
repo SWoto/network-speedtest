2b3c410b0a1740eea08fc739521eedb93b02163b
Bruno <brunostinghel137@gmail.com>
2020-06-28 18:52:22 -0300

    messages to make it more easier to use

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ REPLAY = 60 # time between every measument (do_test_report)
 PLOT_CHART = True # it will plot some chart if you have any data every LOGBRIEF*REPLAY seconds
 
 objSpeedTest = SpeedTestRegister(HOST, TIMEOUT, SHELVNAME, LOGBRIEF, REPLAY, PLOT_CHART)
+print("Running Network Speed Test")
+print("\tAll your LOGS will be saved at \logs folder")
+print("\tAll your CHARTS will be saved at \charts folder with .html format")
+print("\tEvery {} seconds, a new Brief will be logged and a new Chart saved".format(REPLAY*LOGBRIEF))
 
 objScheduler = sched.scheduler(time.time, time.sleep)
 objScheduler.enter(REPLAY, 1, objSpeedTest.do_test_report, (objScheduler,))
