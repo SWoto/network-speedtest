791bdca4e54125cc4b2c4d9c4fb0c4655cb7f1bd
Bruno <brunostinghel137@gmail.com>
2020-06-29 13:03:06 -0300

    added suport to test in more than one network

--- classes/classSpeedTestRegister.py
+++ classes/classSpeedTestRegister.py
@@ -1,5 +1,6 @@
 import sched, time
 import requests
+import json
 
 #persistent, dictionary-like object
 import shelve 
@@ -58,8 +59,16 @@ class SpeedTestRegister(EscritorDeLog):
             with shelve.open(self.shelvename) as db:
                 if self.check_internet_connection() and self.do_speedtest():
                     json_object = self.speedtest.results.dict()
-                else:  
-                    json_object = {"download": 0, "upload": 0,  "ping": 999}
+                else:
+                    # get last used client
+                    # ATENTION: if the first test happens to be offline, 
+                    # it will be empty, and if the you have tested with another
+                    # network and the first one with the new network is offline
+                    # the offline result will be given to the former 
+                    self.escreve_log.warning("You're offline")
+                    key = max(list(db.keys()))
+                    client = db[key].get('client')
+                    json_object = {"download": 0, "upload": 0,  "ping": 999, 'client': client}
                 key = str(int(time.time()))
                 db[key] = json_object
             if self._counter % self.logbrief == 0: 
@@ -104,8 +113,16 @@ class SpeedTestRegister(EscritorDeLog):
         if(_PLOT_CHATS and self.plot_charts):
             df = pd.DataFrame()
             with shelve.open(self.shelvename) as db:
+                clients = {}
                 for measure in db:
-                    df = df.append(db[measure], ignore_index=True)
+                    client_dict = db[measure].get('client')
+                    client_str = json.dumps(client_dict)
+                    if not(client_str in clients) and client_str and client_dict:
+                        clients[client_str] = client_dict.get('ip') + ' - ' + client_dict.get('isp')
+                    if client_str and client_dict:
+                        new_measure = db[measure]
+                        new_measure['client'] = clients[client_str]
+                        df = df.append(new_measure, ignore_index=True)
 
             if len(df.index) > self.logbrief:
                 df = df.sort_values(by=['timestamp'])
@@ -116,24 +133,50 @@ class SpeedTestRegister(EscritorDeLog):
                     shared_xaxes=True, 
                     subplot_titles=("Download", "Upload", "Ping"))
 
-                # add values
-                fig.add_trace(
-                    go.Scatter(x=df['timestamp'], y=df['download']),
-                    row=1, col=1
-                )
-                fig.add_trace(
-                    go.Scatter(x=df['timestamp'], y=df['upload']),
-                    row=2, col=1
-                )
-                fig.add_trace(
-                    go.Scatter(x=df['timestamp'], y=df['ping']),
-                    row=3, col=1
-                )
+                clients = df.client.unique()
+                buttons = []
+                for i, client in enumerate(clients, 1):
+                    df_unique = df[df['client']==client]
+                    fig.add_trace(
+                        go.Scatter(x=df_unique['timestamp'], y=df_unique['download']),
+                        row=1, col=1
+                    )
+                    fig.add_trace(
+                        go.Scatter(x=df_unique['timestamp'], y=df_unique['upload']),
+                        row=2, col=1
+                    )
+                    fig.add_trace(
+                        go.Scatter(x=df_unique['timestamp'], y=df_unique['ping']),
+                        row=3, col=1
+                    )
+
+                    button_dict =   dict(label=client,
+                                        method="update",
+                                        args=[{"visible": [False]*(i-1)*3 + [True]*3 + [False]*(len(clients)-i)*3}]
+                                    )
+                    buttons.append(button_dict)
 
                 # change yaxis label
                 fig.update_yaxes(title_text="[Mb/s]", row=2, col=1)
                 fig.update_yaxes(title_text="[Mb/s]", row=1, col=1)
                 fig.update_yaxes(title_text="[ms]", row=3, col=1)
+
+                fig.update_layout(
+                    margin=dict(t=100, b=0, l=0, r=0),
+                )
+
+                fig.update_layout(
+                    updatemenus=[
+                        dict(
+                            direction="down",
+                            buttons=buttons,
+                            showactive=True,
+                            x=0.58,
+                            xanchor="left",
+                            y=1.08,
+                            yanchor="top"
+                        ),
+                    ])
 
                 plotly.offline.plot(fig, filename='charts/speedtest.html', auto_open=False)
                 #fig.show()
